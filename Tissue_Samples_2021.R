42c9bd31427b67624ce615471445cb4a423b116c
Finn <tlobnow@googlemail.com>
2021-09-05 10:59:28 +0200

    update

---
+++ Tissue_Samples_2021.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(data.table)
+
+Samples <- read.csv("https://raw.githubusercontent.com/fayweb/Field-Trip-Organization/main/Tissue_Samples_2021.csv")
+
+Samples <- Samples %>%
+  mutate(Tube = case_when(Cryo_Tube == T ~ "Cryo",
+                          Tube_2mL  == T ~ "2mL",
+                          Tube_15mL == T ~ "15mL"),
+         Storage_Medium = case_when(Storage_4_K2Cr2O7 == T ~ "Potassium Dichromate",
+                             Storage_minus20_Alcohol  == T ~ "Alcohol",
+                             Storage_minus80          == T ~ "None",
+                             RT_Formalin              == T ~ "Formalin"),
+         Storage_Temp = case_when(Storage_4_K2Cr2O7       == T ~ "+4 degrees",
+                                  Storage_minus20_Alcohol == T ~ "-20 degrees",
+                                  Storage_minus80         == T ~ "-80 degrees",
+                                  RT_Formalin             == T ~ "RT"),
+         Tube_Need = case_when(need_Cryo_tubes > 0 ~ need_Cryo_tubes,
+                               need_2mL_tubes  > 0 ~ need_2mL_tubes,
+                               need_15mL_tubes > 0 ~ need_15mL_tubes),
+         need_to_print = case_when(label_need_reality >= 0 ~ label_need_reality),
+         need_to_rewrite = case_when( expired_Labels > 0 ~ expired_Labels,
+                                      expired_labeled_Cryo > 0 ~ expired_labeled_Cryo,
+                                      expired_labeled_Cryo > 0 | expired_Labels > 0 ~ (expired_labeled_Cryo + expired_Labels))) %>%
+        select(Sample_Name,
+         Label,
+         Person,
+         Storage_Temp,
+         Storage_Medium,
+         Tube,
+         Tube_Need,
+         need_to_print,
+         need_to_rewrite,
+         c(11:21),
+         -need_Cryo_tubes, -need_2mL_tubes, -need_15mL_tubes,
+         -label_need_theoretical, -label_need_reality, 
+         -expired_labeled_Cryo, -expired_Labels)
+ 
+
+  #,
+  #Done = case_when(new_labeled_Cryo > 0 ~ (new_labeled_Cryo / Tube_Need))) 
